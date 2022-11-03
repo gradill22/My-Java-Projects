6659ace4d88082d24923d9521dc52fa92ed0dcd6
Grady Dillon <gradydillon22@gmail.com>
2022-11-03 13:56:45 -0400

    Java Projects
    All of my Java projects since 2018.

--- Data Structures/src/Homework1/Q3.java
+++
@@ -1,8 +0,0 @@
-package Homework1;
-
-public class Q3 {
-    public static void main(String[] args) {
-        String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        System.out.println(uppercase.charAt((int) (Math.random() * uppercase.length())));
-    }
-}
